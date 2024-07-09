f862524505507e6cb9a8718ab28714b50dfd7235
ayushguptasah <ayushguptasah99@gmail.com>
2024-07-09 23:53:56 +0530

    updated

---
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './FoodItem.css'
+
+const FoodItem = ({id,name,price,description,image}) => {
+  return (
+    <div className='food-item'>
+        <div className="food-item-img-container">
+            
+        </div>
+      
+    </div>
+  )
+}
+
+export default FoodItem
