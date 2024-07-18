644e99bbdfed0b244d579a62d6d69a4996e6833c
ayushguptasah <ayushguptasah99@gmail.com>
2024-07-18 23:12:36 +0530

    updated

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import './FoodItem.css'
+import React from "react";
+import "./FoodItem.css";
+import { assets } from "../../assets/assets";
 
-const FoodItem = ({id,name,price,description,image}) => {
+const FoodItem = ({ id, name, price, description, image }) => {
   return (
-    <div className='food-item'>
-        <div className="food-item-img-container">
-            
+    <div className="food-item">
+      <div className="food-item-img-container">
+        <img className="food-item-image" src={image} alt="" />
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+          <img src={assets.rating_starts} alt="" />
         </div>
-      
+        <p className="food-item-desc">{description}</p>
+        <p className="food-item-price">Rs. {price}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FoodItem
+export default FoodItem;
