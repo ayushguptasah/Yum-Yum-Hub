9051a6adeda24f1457d851db408a1e66fa02e1d2
ayushguptasah <ayushguptasah99@gmail.com>
2024-08-05 23:46:19 +0530

    added footer

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,16 +3,17 @@ import "./Home.css";
 import Header from "../../components/Header/Header";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
+import AppDownload from "../../components/AppDownload/AppDownload";
 
 const Home = () => {
-
-    const [category,setCategory] = useState("All");
+  const [category, setCategory] = useState("All");
 
   return (
     <div>
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
-      <FoodDisplay category={category}/>
+      <FoodDisplay category={category} />
+      <AppDownload />
     </div>
   );
 };
